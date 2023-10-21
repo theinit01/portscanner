5c32b39f6b5694f7a6242df9f3fc6b1e87367db9
Rishika631 <ajaystores.korba495677@gmail.com>
2023-10-22 01:31:15 +0530

    added custom timeout functionality

--- portscanner.py
+++ portscanner.py
@@ -11,7 +11,7 @@ RED = '\033[91m'
 RESET = '\033[0m'
 
 usage = """
-Usage: python3 scanner.py <Target> <Start_Port> <End_Port>
+Usage: python scanner.py <Target> <Start_Port> <End_Port>
 
 Description:
   This script is a simple port scanner that checks the status of ports within a specified range on a target host.
@@ -23,13 +23,16 @@ Arguments:
 
   - End_Port: The ending port of the scan range. Specify a port number between 1 and 65535. It should be greater than or equal to the Start_Port.
 
+Optional Arguments:
+  --timeout <Timeout>: Specify a custom timeout value in seconds for port connections. Default is 0.5 seconds.
+
 Example:
-  python3 scanner.py example.com 80 100
-  This will scan ports 80 to 100 on the host 'example.com'.
+  python scanner.py example.com 80 100 --timeout 1.0
+  This will scan ports 80 to 100 on the host 'example.com' with a custom timeout of 1.0 seconds.
 """
 
 
-if (len(sys.argv) != 4):
+if (len(sys.argv) != 6 and len(sys.argv) != 4 ):
     print(usage)
     sys.exit()
 
@@ -38,6 +41,7 @@ def parse_arguments():
     parser.add_argument("Target", help="The target host or IP address to scan.")
     parser.add_argument("Start_Port", type=int, help="The starting port for the scan range.")
     parser.add_argument("End_Port", type=int, help="The ending port for the scan range.")
+    parser.add_argument("--timeout", type=float, default=0.5, help="Custom timeout for port connections (default: 0.5 seconds)")
     return parser.parse_args()
 
 def resolve_target(target):
@@ -55,13 +59,13 @@ def identify_service(port, protocol):
     except (OSError, socket.error):
         return "Unknown"
 
-def scan_ports(target, start_port, end_port):
+def scan_ports(target, start_port, end_port, timeout):
     open_ports = []
     closed_ports = []
     for port in range(start_port, end_port + 1):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(0.5)
+                s.settimeout(timeout)
                 s.connect((target, port))
                 open_ports.append(port)
         except:
@@ -88,7 +92,7 @@ if __name__ == "__main__":
 
     args = parse_arguments()
     target = resolve_target(args.Target)
-    open_ports, closed_ports = scan_ports(target, args.Start_Port, args.End_Port)
+    open_ports, closed_ports = scan_ports(target, args.Start_Port, args.End_Port, args.timeout)
     print_results(target, open_ports, closed_ports, args.Start_Port, args.End_Port)  
       
     end_time = time.time()
